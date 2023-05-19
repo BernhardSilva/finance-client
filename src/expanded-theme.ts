be1808505690e66532c91c19c7b1fd114287843b
bernhard.silva@gmail.com <bernhard.silva@gmail.com>
2023-05-19 09:39:29 -0400

    updated .env deleted from cache and expanded-theme eslint error import fixed

--- src/expanded-theme.ts
+++ src/expanded-theme.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line
-import { Palette, PaletteColor } from '@mui/material/styles/createPalette';
-
 declare module '@mui/material/styles/createPalette' {
 	interface PaletteColor {
 		[key: number]: string;
